72e00aea9a3117badb94a150027aface588aaa58
hirothings <hirothings@sakamotohiroshinoMacBook-Pro.local>
2016-12-18 20:59:48 +0900

    ライブラリ更新

---
+++ QiitaPocket/ViewModel/ArticleListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleListViewModel.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2016/12/18.
+//  Copyright © 2016年 hiroshings. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ArticleListViewModel {
+    
+    let fetchNotification: PublishSubject<Void> = PublishSubject()
+}
