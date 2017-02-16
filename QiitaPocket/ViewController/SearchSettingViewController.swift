246348004b59d266d9778691080ace0814110fe6
hirothings <hiroshings.job1987@gmail.com>
2017-02-16 23:29:59 +0900

    検索画面のViewController追加

---
+++ QiitaPocket/ViewController/SearchSettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchSettingViewController.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/02/11.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import UIKit
+
+class SearchSettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
