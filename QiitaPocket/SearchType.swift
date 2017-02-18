f902be076742dd4b78dab9532cc7190526f7b33d
hirothings <hiroshings.job1987@gmail.com>
2017-02-18 15:47:19 +0900

    検索設定enum追加

---
+++ QiitaPocket/SearchType.swift
@@ -0,0 +1,20 @@
+//
+//  SearchType.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/02/18.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import Foundation
+
+enum SearchSort: String {
+    case recent
+    case popular
+}
+
+enum SearchPeriod: String {
+    case all
+    case month
+    case week
+}
