dc1f27f44e3e23d0a20ccdb0ee4dc72d9d1a9dd8
hirothings <hiroshings.job1987@gmail.com>
2017-02-19 04:37:48 +0900

    UserDefaultsの管理をenumからメソッドの集まりに変更(用途が限られているので、より具体的な型・メソッドにしたかった)

--- QiitaPocket/UserSettings.swift
+++ QiitaPocket/UserSettings.swift
@@ -8,37 +8,52 @@
 
 import Foundation
 
-
-enum UserSettings: String {
+class UserSettings {
     
-    case searchHistory
-    case searchSort
-    case searchPeriod
-    case searchTag
-    
-    // Int
-    func set(value: Int) {
-        UserDefaults.standard.set(value, forKey: self.rawValue)
+    // 検索タグ
+    static func getCurrentSearchTag() -> String {
+        UserDefaults.standard.register(defaults: ["CurrentSearchTag": ""])
+        return UserDefaults.standard.string(forKey: "CurrentSearchTag")!
     }
-    func integer() -> Int {
-        return UserDefaults.standard.integer(forKey: self.rawValue)
+    static func setCurrentSearchTag(name: String) {
+        UserDefaults.standard.set(name, forKey: "CurrentSearchTag")
     }
     
-    // String
-    func string() -> String? {
-        return UserDefaults.standard.string(forKey: self.rawValue)
+    // 検索履歴
+    static func getSearchHistory() -> [String] {
+        UserDefaults.standard.register(defaults: ["SearchHistory": []])
+        return UserDefaults.standard.object(forKey: "SearchHistory") as! Array<String>
+    }
+    static func setSearchHistory(list: [String]) {
+        UserDefaults.standard.set(list, forKey: "SearchHistory")
     }
     
-    // Object
-    func set(value: Any) {
-        UserDefaults.standard.set(value, forKey: self.rawValue)
+    // 検索順
+    static func getSearchSort() -> SearchSort? {
+        guard let rawValue = UserDefaults.standard.string(forKey: "SearchSort") else {
+            return nil
+        }
+        return SearchSort(rawValue: rawValue)
     }
-    func object() -> Any? {
-        return UserDefaults.standard.object(forKey: self.rawValue)
+    static func setSearchSort(_ searchSort: SearchSort) {
+        UserDefaults.standard.set(searchSort.rawValue, forKey: "SearchHistory")
+        UserDefaults.standard.object(forKey: "SearchHistory")
+    }
+    
+    // 期間
+    static func getSearchPeriod() -> SearchPeriod? {
+        guard let rawValue = UserDefaults.standard.string(forKey: "SearchPeriod") else {
+            return nil
+        }
+        return SearchPeriod(rawValue: rawValue)
+    }
+    static func setSearchPeriod(_ searchPeriod: SearchPeriod) {
+        UserDefaults.standard.set(searchPeriod.rawValue, forKey: "SearchPeriod")
+        UserDefaults.standard.object(forKey: "SearchPeriod")
     }
     
     // delete
-    func delete() {
-        UserDefaults.standard.removeObject(forKey: self.rawValue)
+    static func delete(for key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
     }
 }
