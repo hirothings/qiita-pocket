78333067e64bde433b13a321fb32dff8292d3bf4
hirothings <hiroshings.job1987@gmail.com>
2017-02-26 00:12:31 +0900

    ArrayにisNotEmptyプロパティ追加

---
+++ QiitaPocket/ArrayExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ArrayExtension.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/02/26.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
