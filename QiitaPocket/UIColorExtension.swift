7a906c1f8fff944c0b933df9dd695d8fd74813f8
hirothings <hiroshings.job1987@gmail.com>
2017-02-26 15:43:34 +0900

    UIColorのExtension追加

---
+++ QiitaPocket/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/02/26.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let theme = #colorLiteral(red: 0.3490196078, green: 0.7333333333, blue: 0.04705882353, alpha: 1)
+}
