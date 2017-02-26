5aa5eae36be3378ae43cb1bda7e461ce24d4e0b2
hirothings <hiroshings.job1987@gmail.com>
2017-02-26 18:29:59 +0900

    RequestProtocol追加。関連型にJSONDecodableプロトコルを指定

---
+++ QiitaPocket/QiitaRequest.swift
@@ -0,0 +1,30 @@
+//
+//  QiitaRequest.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/02/26.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol QiitaRequest {
+    
+    associatedtype ResponseObject: JSONDecodable
+    
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: [String: Any]? { get }
+}
+
+extension QiitaRequest {
+    var baseURL: String {
+        return "https://qiita.com/api/v2"
+    }
+    
+    var method: HTTPMethod {
+        return Alamofire.HTTPMethod.get
+    }
+}
