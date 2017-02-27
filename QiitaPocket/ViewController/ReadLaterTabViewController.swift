74ff147974b3d4965b0b2d937c63abeac30d36cb
hirothings <hiroshings.job1987@gmail.com>
2017-02-27 20:16:29 +0900

    タブメニューをインスタっぽい見た目に調整

--- QiitaPocket/ViewController/ReadLaterTabViewController.swift
+++ QiitaPocket/ViewController/ReadLaterTabViewController.swift
@@ -12,6 +12,24 @@ import XLPagerTabStrip
 class ReadLaterTabViewController: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = .black
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 1.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        
+        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            
+            oldCell?.label.textColor = UIColor.black.withAlphaComponent(0.6)
+            newCell?.label.textColor = .black
+        }
+
         super.viewDidLoad()
 
         buttonBarView.removeFromSuperview()
