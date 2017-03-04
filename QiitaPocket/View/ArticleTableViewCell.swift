776bcccc0ddbcf5448c1aff527a42b5ce7d886fa
hirothings <hiroshings.job1987@gmail.com>
2017-03-04 22:36:34 +0900

    ArticleTableViewCellのスワイプアニメをprotocolに批准する書き方に変更 UIPanGestureRecognizerのイベント通知をRX化

--- QiitaPocket/View/ArticleTableViewCell.swift
+++ QiitaPocket/View/ArticleTableViewCell.swift
@@ -10,12 +10,7 @@ import UIKit
 import WebImage
 import RxSwift
 
-protocol SwipeCellDelegate: class {
-    func didSwipeCell(at indexPath: IndexPath)
-}
-
-class ArticleTableViewCell: UITableViewCell {
-    
+final class ArticleTableViewCell: UITableViewCell, SwipeCellType {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
@@ -23,9 +18,11 @@ class ArticleTableViewCell: UITableViewCell {
     @IBOutlet weak var cardView: UIView!
     
     var swipeGesture = UIPanGestureRecognizer()
+    var swipeIndexPath: IndexPath = IndexPath()
+    var preTransration: CGPoint?
     
     weak var delegate: SwipeCellDelegate?
-        
+    
     var article: Article! {
         didSet {
             self.titleLabel.text = article.title
@@ -35,72 +32,18 @@ class ArticleTableViewCell: UITableViewCell {
         }
     }
     
-    private var swipeIndexPath: IndexPath = IndexPath()
-    private var preTransration: CGPoint?
+    private var bag = DisposeBag()
 
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
-        swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(self.onRightSwipe(_:)))
+        swipeGesture.rx.event.bindNext { [weak self] (gesture: UIPanGestureRecognizer) in
+            self?.onRightSwipe(gesture)
+        }
+        .addDisposableTo(bag)
         swipeGesture.delegate = self
         self.cardView.addGestureRecognizer(swipeGesture)
     }
-    
-    // TODO: protocol化
-    func onRightSwipe(_ gesture: UIPanGestureRecognizer) {
-        self.contentView.backgroundColor = UIColor.theme
-        let translation = gesture.translation(in: self)
-
-        guard let tableView = self.superview?.superview as? UITableView else { return }
-        
-        switch gesture.state {
-        case .began:
-            let swipeLocation: CGPoint = gesture.location(in: tableView)
-            swipeIndexPath = tableView.indexPathForRow(at: swipeLocation)!
-
-        case .changed:
-            // 左端より先にはスワイプさせない
-            if translation.x < 0 { return }
-            
-            // トランジション方向が、-x方向の場合、閾値を考慮せずカードを移動させる
-            if let preTransration = preTransration {
-                if translation.x < preTransration.x {
-                    self.cardView.frame.origin.x = translation.x
-                }
-            }
-
-            // トランジション方向が閾値を超えた場合、セルを右スワイプ中とみなす
-            if 30.0 < translation.x {
-                preTransration = translation
-                self.cardView.frame.origin.x = translation.x
-                tableView.panGestureRecognizer.isEnabled = false // 右スワイプ中はtableviewのscrollを切る
-            }
-
-        case .ended:
-            if 80.0 < translation.x {
-                UIView.animate(
-                    withDuration: 0.1,
-                    animations: { [unowned self] in
-                        self.cardView.frame.origin.x = UIScreen.main.bounds.width
-                    },
-                    completion: { [unowned self] _ in
-                        self.delegate?.didSwipeCell(at: self.swipeIndexPath)
-                    })
-            }
-            else {
-                UIView.animate(withDuration: 0.1, animations: { [unowned self] in
-                    self.cardView.frame.origin.x = 0
-                })
-            }
-            tableView.panGestureRecognizer.isEnabled = true // tableviewのscrollを復帰する
-            preTransration = nil
-        default:
-            break
-        }
-
-    }
-    
     
     override func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
