664f1561bb2a066b54e7d44af270f634f0c2c62e
hirothings <hiroshings.job1987@gmail.com>
2017-03-14 21:33:29 +0900

    タブアイコンを表示

--- QiitaPocket/ViewController/MainTabBarController.swift
+++ QiitaPocket/ViewController/MainTabBarController.swift
@@ -13,7 +13,13 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let searchButton = tabBar.items?.first else { return }
+        guard let readLaterButton = tabBar.items?.last else { return }
+        
+        searchButton.image = #imageLiteral(resourceName: "ic-tab-search").withRenderingMode(.alwaysOriginal)
+        readLaterButton.image = #imageLiteral(resourceName: "ic-tab-read-later").withRenderingMode(.alwaysOriginal)
+        searchButton.selectedImage = #imageLiteral(resourceName: "ic-tab-search_selected").withRenderingMode(.alwaysOriginal)
+        readLaterButton.selectedImage = #imageLiteral(resourceName: "ic-tab-read-later_selected").withRenderingMode(.alwaysOriginal)
     }
 
 }
