ccfe5d31aef6cdc0cc7559b495917eb9bb9ff925
hirothings <hiroshings.job1987@gmail.com>
2017-04-02 18:32:59 +0900

    その他ビュー追加

---
+++ QiitaPocket/ViewController/OtherTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OtherTableViewController.swift
+//  QiitaPocket
+//
+//  Created by hirothings on 2017/04/02.
+//  Copyright © 2017年 hirothings. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class OtherTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "その他"
+    }
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        switch indexPath.row {
+//        case 0:
+//            <#code#>
+//        default:
+//            <#code#>
+//        }
+//    }
+
+}
