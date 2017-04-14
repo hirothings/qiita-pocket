082d842eb169e552d1f4d146ab75ecfda3d1b119
hirothings <hiroshings.job1987@gmail.com>
2017-04-15 08:37:58 +0900

    Out of lange時、nilを返すExtensionを追加

--- QiitaPocket/Extension/ArrayExtension.swift
+++ QiitaPocket/Extension/ArrayExtension.swift
@@ -13,3 +13,10 @@ extension Array {
         return !self.isEmpty
     }
 }
+
+extension Collection where Indices.Iterator.Element == Index {
+    
+    subscript(safe index: Index) -> Generator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
