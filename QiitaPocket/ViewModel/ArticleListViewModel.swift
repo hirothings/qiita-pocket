23131314442c64f3f074095a45393117b89308a3
hirothings <hiroshings.job1987@gmail.com>
2017-05-13 20:56:21 +0900

    旧viewModelの処理のうち、最新記事取得用の処理をviewModelに分割

--- QiitaPocket/ViewModel/ArticleListViewModel.swift
+++ QiitaPocket/ViewModel/ArticleListViewModel.swift
@@ -111,53 +111,6 @@ class ArticleListViewModel: FetchArticleType {
     }
     
     
-    func configureRecentArticle() {
-        fetchRecentTrigger
-            .do(onNext: { [unowned self] tuple in
-                self.isLoading.value = true
-                self.searchBarTitle.value = tuple.keyword // TODO: 検索設定追加
-            })
-            .flatMap { tuple in
-                Articles.fetch(with: tuple.keyword, page: tuple.page)
-            }
-            .do(onNext: { [unowned self] _ in
-                self.isLoading.value = false
-            })
-            .observeOn(Dependencies.sharedInstance.mainScheduler)
-            .subscribe(
-                onNext: { [weak self] (model: Articles) in
-                    guard let `self` = self else { return }
-                    let _articles = model.items
-                    if _articles.isNotEmpty {
-                        self.hasData.value = true
-                        let addedStateArticles = self.addReadLaterState(_articles)
-                        self.articles.value += addedStateArticles
-                    }
-                    else {
-                        self.hasData.value = false
-                    }
-                },
-                onError: { (error) in
-                    // TODO: 判定が面倒なので、errorの種類自体をEnumにする
-                    switch error {
-                    case let qiitaError as QiitaAPIError:
-                        self.showAlert(message: qiitaError.message)
-                    case let connectionError as ConnectionError:
-                        self.showAlert(message: connectionError.message)
-                    default:
-                        break
-                    }
-                    self.isLoading.value = false
-                    self.configureRecentArticle() // Disposeが破棄されるので、再度設定する
-                },
-                onCompleted: {
-                    print("Completed")
-                }
-            )
-            .addDisposableTo(bag)
-    }
-    
-    
     // MARK: - Private Method
 
     /// ストック順に記事をソートする
