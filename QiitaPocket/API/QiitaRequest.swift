d1058f4ba5d7e194e30f4308a2cef0850376b823
hirothings <hiroshings.job1987@gmail.com>
2017-09-04 22:54:16 +0900

    pathまで消してた

--- QiitaPocket/API/QiitaRequest.swift
+++ QiitaPocket/API/QiitaRequest.swift
@@ -13,6 +13,7 @@ protocol QiitaRequest {
     associatedtype ResponseObject: JSONDecodable
     
     var baseURL: String { get }
+    var path: String { get }
     var method: HTTPMethod { get }
     var parameters: [String: Any]? { get }
 }
